c66cbf85f77beccff38d7ea92a08aa07e1bcc7d0
Rahulg321 <rg5353070@gmail.com>
2025-10-21 16:11:21 +0530

    added a console.log

--- apps/frontend/app/not-found.tsx
+++ apps/frontend/app/not-found.tsx
@@ -4,6 +4,8 @@ import { redirect } from "next/navigation";
 import React from "react";
 
 const NotFoundPage = () => {
+  console.log("Not Found Page");
+
   return (
     <div>
       <h2>Not Found</h2>
